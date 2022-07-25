103ec9b26de11b09c1999eab81c59edc1dba6e66
Poorav SHARMA <30045900@tafe.wa.edu.au>
2022-07-25 14:05:37 +0800

    Version 1

---
+++ Astronomical_Processing/Astronomical_Processing.cs
@@ -0,0 +1,18 @@
+using Galileo;
+using Galileo6;
+
+namespace Astronomical_Processing
+{
+    public partial class Astronomical_Processing : Form
+    {
+        public Astronomical_Processing()
+        {
+            InitializeComponent();
+        }
+
+        private void Astronomical_Processing_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
